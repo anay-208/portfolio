0c0ebb77bf6db0f00142e10f59fb84183c2149b2
Anay <anayparswani2008@gmail.com>
2023-12-22 20:05:46 +0530

    Bug fixes

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -3,18 +3,17 @@
 import { useState, useRef, useEffect } from "react";
 
 type Section = {
-  name: string, 
-  active: boolean
-}
+  name: string;
+  active: boolean;
+};
 
 export default function Navbar() {
   const [mobileMenuOpened, setMobileMenuOpened] = useState(false);
 
-
-  const [sections, setSections ] = useState<Section[]>([
-    {name: 'introduction', active: true},
-   {name: 'projects', active: false}
-  ])
+  const [sections, setSections] = useState<Section[]>([
+    { name: "introduction", active: true },
+    { name: "projects", active: false },
+  ]);
   const [scrolled, setScrolled] = useState(false);
 
   useEffect(() => {
@@ -22,26 +21,27 @@ export default function Navbar() {
       setScrolled(window.pageYOffset > window.innerHeight / 2);
     };
 
-    window.addEventListener('scroll', checkScroll);
+    window.addEventListener("scroll", checkScroll);
 
     return () => {
-      window.removeEventListener('scroll', checkScroll);
+      window.removeEventListener("scroll", checkScroll);
     };
   }, []);
 
-  
   const observers = useRef<IntersectionObserver[]>([]);
-  
+
   useEffect(() => {
     observers.current = sections.map((section, index) => {
       const observer = new IntersectionObserver(
         ([entry]) => {
-          setSections((sections) => sections.map((section, i) => ({
-            ...section,
-            active: i === index ? entry.isIntersecting : section.active,
-          })));
+          setSections((sections) =>
+            sections.map((section, i) => ({
+              ...section,
+              active: i === index ? entry.isIntersecting : section.active,
+            })),
+          );
         },
-        { threshold: 0.5 }
+        { threshold: 0.5 },
       );
 
       const element = document.getElementById(section.name);
@@ -55,11 +55,13 @@ export default function Navbar() {
     return () => observers.current.forEach((observer) => observer.disconnect());
   }, [sections]);
 
-
-
   return (
-    <nav className="fixed font-default tracking-wide w-full z-50">
-      <div className={`transition duration-500 relative z-20 mx-auto max-w-7xl ${scrolled ? 'bg-gray-800/50' : 'bg-gray-800/25'} px-2 sm:px-6 lg:px-8`}>
+    <nav className="fixed z-50 w-full font-default tracking-wide">
+      <div
+        className={`relative z-20 mx-auto max-w-7xl transition duration-500 ${
+          scrolled ? "bg-gray-800/50" : "bg-gray-800/25"
+        } px-2 sm:px-6 lg:px-8`}
+      >
         <div className="relative flex h-16 items-center justify-between">
           <div className="absolute inset-y-0 left-0 z-10 flex items-center sm:hidden">
             {/* Mobile menu button*/}
@@ -114,15 +116,20 @@ export default function Navbar() {
             <div className="hidden sm:ml-6 sm:block">
               <div className="flex space-x-4">
                 {/* Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" */}
-                {sections.map(section => (
+                {sections.map((section) => (
                   <a
-                  key={section.name}
-                  href={`#${section.name}`}
-                  className={`rounded-md ${section.active ? "bg-gray-900 text-white" : "text-gray-300 hover:bg-gray-700 hover:text-white"} px-3 py-2 text-sm font-medium`}
-                  aria-current="page"
-                >
-                  {section.name.charAt(0).toUpperCase() + section.name.slice(1)}
-                </a>
+                    key={section.name}
+                    href={`#${section.name}`}
+                    className={`rounded-md ${
+                      section.active
+                        ? "bg-gray-900 text-white"
+                        : "text-gray-300 hover:bg-gray-700 hover:text-white"
+                    } px-3 py-2 text-sm font-medium`}
+                    aria-current="page"
+                  >
+                    {section.name.charAt(0).toUpperCase() +
+                      section.name.slice(1)}
+                  </a>
                 ))}
                 {/* <a
                   href="#"
@@ -159,31 +166,16 @@ export default function Navbar() {
         id="mobile-menu"
       >
         <div className="space-y-1 px-2 pb-3 pt-2 ">
-          <a
-            href="#"
-            className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
-            aria-current="page"
-          >
-            Dashboard
-          </a>
-          <a
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Team
-          </a>
-          <a
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Projects
-          </a>
-          <a
-            href="#"
-            className="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
-          >
-            Calendar
-          </a>
+          {sections.map((section) => (
+            <a
+              href={`#${section.name}`}
+              key={section.name}
+              className="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+              aria-current="page"
+            >
+              {section.name.charAt(0).toUpperCase() + section.name.slice(1)}
+            </a>
+          ))}
         </div>
       </div>
     </nav>
