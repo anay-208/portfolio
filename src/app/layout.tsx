bbe1f5c9e42bd85e15c653c8e73df5001020ed93
Anay <anayparswani2008@gmail.com>
2024-03-25 00:32:35 +0530

    add policies

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,27 +1,22 @@
 import type { Metadata } from "next";
 import { Inter, Oswald } from "next/font/google";
-import localFont from 'next/font/local'
+import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "../components/navbar";
-import { config } from '@fortawesome/fontawesome-svg-core'
-import '@fortawesome/fontawesome-svg-core/styles.css'
+import { config } from "@fortawesome/fontawesome-svg-core";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 import Footer from "@/components/footer";
-config.autoAddCss = false
-const inter = Inter({ subsets: ["latin"] } );
+config.autoAddCss = false;
+const inter = Inter({ subsets: ["latin"] });
 
-
-
-
-
-const oswald = Oswald({ subsets: ["latin"], variable: "--font-default"  });
-const sharlz = localFont({ src: "./hagrid.woff2", variable: "--font-sharlz"})
-const bauhmas93 = localFont({src: "./bauhmas93.woff2", variable: "--font-dkvk"})
+const oswald = Oswald({ subsets: ["latin"], variable: "--font-default" });
+const sharlz = localFont({ src: "./hagrid.woff2", variable: "--font-sharlz" });
+const bauhmas93 = localFont({
+  src: "./bauhmas93.woff2",
+  variable: "--font-dkvk",
+});
 // For variable font
-const fonts = [oswald, sharlz, bauhmas93]
-
-
-
-
+const fonts = [oswald, sharlz, bauhmas93];
 
 export const metadata: Metadata = {
   title: "Anay",
@@ -35,7 +30,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${fonts.map(font => font.variable).join(' ')} ${inter.className}`}>
+      <body
+        className={`${fonts.map((font) => font.variable).join(" ")} ${
+          inter.className
+        }`}
+      >
         <Navbar />
         {children}
         <Footer />
