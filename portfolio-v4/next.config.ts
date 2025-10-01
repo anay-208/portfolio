346ead2b8a17940a86750b51062aba2eb907f86d
anay-208 <me@anayparaswani.dev>
2025-10-01 12:59:59 +0800

    disable eslint for v4*

--- portfolio-v4/next.config.ts
+++ portfolio-v4/next.config.ts
@@ -2,6 +2,11 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+
+  // todo remove
+  eslint: {
+    ignoreDuringBuilds: true,
+  }
 };
 
 export default nextConfig;
