8d66000523393b043f22d544dcdb57b094bced5b
anay-208 <me@anayparaswani.dev>
2026-05-31 23:47:03 +0530

    balance out the margin

--- portfolio-v5/src/app/blogs/layout.tsx
+++ portfolio-v5/src/app/blogs/layout.tsx
@@ -14,7 +14,8 @@ export default async function BlogsLayout({
             <div className="max-w-2xl mx-auto flex flex-col gap-6 min-h-screen px-8 pb-16">
                 <Header posts={posts}/>
                 <main className="flex flex-col gap-16">
-                    <section className=" text-white/75 font-inter">
+                    {/* The first paragraph has a my-4, but margin at the top is not needed. hence -mt-4 is added to balance it */}
+                    <section className=" text-white/75 font-inter -mt-4"> 
                         {children}
                     </section>
                 </main>
